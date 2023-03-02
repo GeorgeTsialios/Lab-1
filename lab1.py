ee18e56f2348a968820ede77d148a3a9196a1483
ΓΕΩΡΓΙΟΣ ΤΣΙΑΛΙΟΣ <up1072868@upnet.gr>
2023-03-02 13:22:14 +0200

    lab1 v1

--- lab1.py
+++ lab1.py
@@ -3,5 +3,6 @@ import requests
 url = input('Write here a URL: ')
 
 with requests.get(url) as response:
-    print(response.headers)
+    for header in response.headers:
+        print(header + " : " + response.headers[header])
 
